97d40ead619db9dbc5fbba13632b6bc5ad57566b
jamesj <ehddnr177@naver.com>
2020-08-25 10:36:14 +0900

    Add debug symbol

--- main.c
+++ main.c
@@ -7,12 +7,14 @@
 #define FIXED_COLUMN	3
 /** 곱셉 중간 과정 출력 라인 최대 개수 */
 #define MAX_LINE		4
+/** 디버깅 여부 */
+#define DEBUG			1
 
 /** 함수 반환값 열거형 */
 enum STATUS{
 	UNKNOWN		= -1,	/** 알 수 없는 값 입력 */
-	FAIL		= 0,	/** 실패 */
-	SUCCESS,			/** 성공 */
+	FAIL 		= 0,	/** 실패 */
+	SUCCESS				/** 성공 */
 };
 
 /////////////////////////////////////////////////////
@@ -25,6 +27,13 @@ enum STATUS{
  * @return 검사 성공 여부, 성공 시 SUCCESS, 실패 시 FAIL 반환
  */
 int check_column( const int val){
+	if( val == 0){
+#if DEBUG
+		printf("\t| ! 0 에 대한 자리수 검사 불가능 in check_column function\n");
+#endif
+		return FAIL;
+	}
+
 	int temp_val = val;
 	int return_value = FAIL;
 	int column_count = 0;
@@ -48,6 +57,13 @@ int check_column( const int val){
  * @return 입력 성공 여부, 성공 시 SUCCESS, 실패 시 FAIL 반환
  */
 int input_number( int *val){
+	if( val == NULL){
+#if DEBUG
+		printf("\t| ! 알 수 없는 주소 참조(NULL) in input_number function\n");
+#endif
+		return FAIL;
+	}
+
 	int return_value_input = scanf( "%d", val);
 	int return_value_column = FAIL;
 
@@ -72,6 +88,13 @@ int input_number( int *val){
  * @return 입력 성공 여부, 성공 시 SUCCESS, 실패 시 FAIL 반환
  */
 void input_numbers( int *number, int *multiply){
+	if( number == NULL || multiply == NULL){
+#if DEBUG
+		printf("\t| ! 알 수 없는 주소 참조(NULL) in input_numbers function\n");
+#endif
+		return ;
+	}
+
 	int return_value = FAIL;
 
 	// 곱해지는 값 입력
@@ -108,11 +131,11 @@ int main(){
 	int current_multiply = 0;
 	// 곱하는 값의 현재 자리수
 	int current_multiply_column = 1;
-	// m 을 출력할 때 라인의 공백 관리
+	// current_multiply 를 출력할 때 라인의 공백 관리
 	int current_print_space = 6;
 	// 곱하는 값과 곱해지는 값을 곱한 값(최종 곱셈 값)
 	int total_result = 0;
-	// m 을 출력할 때의 출력 양식
+	// current_multiply 를 출력할 때의 출력 양식
 	char print_format[32] ={0,};
 
 	// 1. 곱셈에 사용될 두 숫자를 입력받는다.
